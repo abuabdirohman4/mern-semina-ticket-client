a40173a5cc361c7aa901c17962d135f1f5ce6343
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-07-30 20:35:03 +0700

    day 12 : simple spa

--- src/app.js
+++ src/app.js
@@ -1,10 +1,40 @@
+import * as React from "react";
+import { BrowserRouter, Link, Routes, Route, } from "react-router-dom";
 import './App.css';
+
+function Home() {
+    return <h1>Home</h1>
+}
+
+function Categories() {
+    return <h1>Categories</h1>
+}
+
+function About() {
+    return <h1>About</h1>
+}
 
 function App() {
     return (
-        <>
-
-        </>
+        // 'hello'
+        <BrowserRouter>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/categories">Categories</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/categories" element={<Categories />}/>
+                <Route path="/about" element={<About />}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
