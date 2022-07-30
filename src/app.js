cc18dec9484bbc49e141f3b376f14974743b8259
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-07-30 20:44:11 +0700

    day 12 : useParams

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter, Link, Routes, Route, } from "react-router-dom";
+import { BrowserRouter, Link, Routes, Route, useParams} from "react-router-dom";
 import './App.css';
 
 function Home() {
@@ -7,7 +7,34 @@ function Home() {
 }
 
 function Categories() {
-    return <h1>Categories</h1>
+    return (
+        <>
+            <h1>Categories</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Categories</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td><Link to="/categories/56789">Seminar</Link></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td><Link to="/categories/1234">Musik</Link></td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+function CategoriesDetail() {
+    let { id } = useParams();
+    return <h1>Categories : {id}</h1>
 }
 
 function About() {
@@ -30,9 +57,10 @@ function App() {
                 </li>
             </ul>
             <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/categories" element={<Categories />}/>
-                <Route path="/about" element={<About />}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="/categories/:id" element={<CategoriesDetail />} />
+                <Route path="/about" element={<About />} />
             </Routes>
         </BrowserRouter>
     );
