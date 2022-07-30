cab9d49ee04859290717eef2694aff97a703757e
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-07-30 22:25:30 +0700

    day 12 : add form sign in

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+// export default function Button({ onClick, children }) {
+//     return <button onClick={onClick}>{children}</button>
+// }
 
-export default function Button({ onClick, children }) {
-    return <button onClick={onClick}>{children}</button>
+export default function SButton({
+    children,
+    action,
+    variant,
+    size,
+    loading,
+    disabled,
+    className
+}) {
+    return (
+        <Button
+            className={className}
+            onClick={action}
+            variant={variant}
+            disabled={disabled}
+            size={size}
+        >
+            {loading ? 'Loading...' : children}
+        </Button>
+    )
 }
