ceedc21e6a590ebbd71998d43bed605dac52ebc5
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-08-10 11:20:30 +0700

    day 14 : redux install, setup & implement on auth

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import App from "./examples/App11";
+import App from "./App";
+// import App from "./examples/App11";
 // import App from "./examples/App12";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
