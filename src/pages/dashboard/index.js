1a7ebff4e0c5f5e214340b8bea339d0d13e5ece3
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-08-11 16:09:07 +0700

    day 14 : undestanding guard route & redux state

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import SBreadCrumb from "../../components/Breadcrumb";
 
 export default function Dashboard() {
@@ -7,12 +8,24 @@ export default function Dashboard() {
   // console.log(token);
   // if (!token) return <Navigate to="/signin" replace={true} />;
 
+  // Check isi dari redux auth
+  // const state = useSelector((state) => state);
+  // const state = useSelector((state) => state.auth);
+  // console.log("state");
+  // console.log(state);
+
+  // const { token } = useSelector((state) => state.auth);
+  // console.log("token");
+  // console.log(token);
+
+  // const { role } = useSelector((state) => state.auth);
+  // console.log("role");
+  // console.log(role);
+
   return (
-    <>
-      <Container className="mt-3">
-        <SBreadCrumb />
-        <h1>Dashboard</h1>
-      </Container>
-    </>
+    <Container className="mt-3">
+      <SBreadCrumb />
+      <h1>Dashboard</h1>
+    </Container>
   );
 }
